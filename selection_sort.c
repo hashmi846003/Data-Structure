b7a8890296187ae94d77b9ec61f928baeb152de4
Fakhruddin Hashmi <109304853+hashmi846003@users.noreply.github.com>
2025-12-21 23:23:39 +0530

    selection sort

--- selection_sort.c
+++ selection_sort.c
@@ -11,11 +11,6 @@ int main() {
         scanf("%d", &arr[i]);
     }
 
-    printf("Array before sorting:\n");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-
     // Selection Sort
     for (int i = 0; i < size - 1; i++) {
         int minIndex = i;
@@ -26,15 +21,12 @@ int main() {
             }
         }
 
-        // Swap
-        if (minIndex != i) {
-            int temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
-        }
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
     }
 
-    printf("\nArray after selection sort:\n");
+    printf("Array after selection sort:\n");
     for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
     }
