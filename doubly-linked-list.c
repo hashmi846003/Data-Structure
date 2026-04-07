a6fe35a0feb26a087fadd429ee550768a28fe8a7
Fakhruddin Hashmi <109304853+hashmi846003@users.noreply.github.com>
2026-04-07 18:24:55 +0530

    stack

--- doubly-linked-list.c
+++ doubly-linked-list.c
@@ -1,60 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-struct node
-{
+struct Node {
     int data;
-    struct node *next;
-    struct node *prev;
+    struct Node* prev;
+    struct Node* next;
 };
 
-int main()
-{
-    // Create and initialize nodes
-    struct node *head;
-    struct node *one = NULL;
-    struct node *two = NULL;
-    struct node *three = NULL;
-    struct node *current = NULL;
+void insertFront(struct Node** head, int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->prev = NULL;
+    newNode->next = (*head);
 
-    // Allocate memory for the nodes
-    one = (struct node*) malloc(sizeof(struct node));
-    two = (struct node*) malloc(sizeof(struct node));
-    three = (struct node*) malloc(sizeof(struct node));
-
-    // Assign data to nodes
-    one->data = 1;
-    two->data = 2;
-    three->data = 3;
-
-    // Connect first node
-    // with the second node
-    one->next = two;
-    
-    // Connect second node
-    // with the third node
-    two->next = three;
-
-    // make next pointer of
-    //third node to NULL
-    //indicates last node
-    three->next = NULL;
-
-    //connect previous nodes
-    one->prev = NULL;
-    two->prev = one;
-    three->prev = two;
-   
-    // Save address of first node in head
-    head = one;
-    current = head;
-
-    // print the linked list values forward
-    while(current != NULL)
-    {
-        printf("%d ", current->data);
-        current = current->next;
+    if ((*head) != NULL) {
+        (*head)->prev = newNode;
     }
+
+    (*head) = newNode;
+}
+
+void insertEnd(struct Node** head, int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* last = *head;
+
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (*head == NULL) {
+        newNode->prev = NULL;
+        *head = newNode;
+        return;
+    }
+
+    while (last->next != NULL) {
+        last = last->next;
+    }
+
+    last->next = newNode;
+    newNode->prev = last;
+}
+
+void deleteNode(struct Node** head, struct Node* delNode) {
+    if (*head == NULL || delNode == NULL) {
+        return;
+    }
+
+    if (*head == delNode) {
+        *head = delNode->next;
+    }
+
+    if (delNode->next != NULL) {
+        delNode->next->prev = delNode->prev;
+    }
+
+    if (delNode->prev != NULL) {
+        delNode->prev->next = delNode->next;
+    }
+
+    free(delNode);
+}
+
+void printList(struct Node* node) {
+    struct Node* last;
+    printf("Traversal in forward direction:\n");
+    while (node != NULL) {
+        printf("%d ", node->data);
+        last = node;
+        node = node->next;
+    }
+    printf("\nTraversal in reverse direction:\n");
+    while (last != NULL) {
+        printf("%d ", last->data);
+        last = last->prev;
+    }
+    printf("\n");
+}
+int main() {
+    struct Node* head = NULL;
+
+    insertEnd(&head, 6);
+    insertFront(&head, 7);
+    insertFront(&head, 1);
+    insertEnd(&head, 4);
+
+    printf("Created Doubly Linked List:\n");
+    printList(head);
+
+    deleteNode(&head, head->next);
+
+    printf("List after deletion of second node:\n");
+    printList(head);
 
     return 0;
 }
