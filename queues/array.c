f763da2ebfbaee21d26c6f3821e6e6027a680b5f
Fakhruddin Hashmi <109304853+hashmi846003@users.noreply.github.com>
2026-04-13 08:58:16 +0530

    queue

---
+++ queues/array.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Queue
+{
+    int size;
+    int front;
+    int rear;
+    int *Q;
+};
+void create(struct Queue *q,int size)
+{
+    q->size=size;
+    q->front=q->rear=-1;
+    q->Q=(int *)malloc(q->size*sizeof(int));
+}
+void enqueue(struct Queue *q,int x)
+{
+    if(q->rear==q->size-1)
+        printf("Queue is Full");
+    else
+    {
+        q->rear++;
+        
+        q->Q[q->rear]=x;
+    }
+}
+int dequeue(struct Queue *q)
+{
+    int x=-1;
+    
+    if(q->front==q->rear)
+        printf("Queue is Empty\n");
+    else
+    {
+        q->front++;
+        x=q->Q[q->front];
+    }
+    return x;
+}
